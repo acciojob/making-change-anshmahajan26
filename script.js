4eced2cdc234ab90528e67f87b93ca430c384d8b
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-13 07:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const makeChange = (c) => {
-  // your name here
+  let result = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  // quarters
+  result.q = Math.floor(c / 25);
+  c = c % 25;
+
+  // dimes
+  result.d = Math.floor(c / 10);
+  c = c % 10;
+
+  // nickels
+  result.n = Math.floor(c / 5);
+  c = c % 5;
+
+  // pennies
+  result.p = c;
+
+  return result;
 };
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
